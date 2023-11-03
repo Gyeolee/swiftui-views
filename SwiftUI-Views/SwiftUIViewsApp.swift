66d68635ed7cc55aef0b8fd918a750751f36c155
Gyeolee <gyeoleeeeeee@gmail.com>
2023-11-03 11:09:29 +0900

    create project

---
+++ SwiftUI-Views/SwiftUIViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewsApp.swift
+//  SwiftUI-Views
+//
+//  Created by Hangyeol on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
