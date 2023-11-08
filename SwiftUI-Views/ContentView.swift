05154c98782c6fe9af158d9b601a6d72d3c4547a
Gyeolee <gyeoleeeeeee@gmail.com>
2023-11-08 13:02:50 +0900

    [ADD] ObserveOffsetScrollView 추가

--- SwiftUI-Views/ContentView.swift
+++ SwiftUI-Views/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var scrollViewOffset: CGPoint = .zero
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ObserveOffsetScrollView(offset: $scrollViewOffset) {
+            VStack {
+                ForEach(Array(0..<10), id: \.self) { _ in
+                    Rectangle()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 100)
+                        .background(Color(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), opacity: 1))
+                }
+            }
         }
-        .padding()
+        .onChange(of: scrollViewOffset) {
+            print(scrollViewOffset)
+        }
     }
 }
 
---
+++ SwiftUI-Views/Views/ObserveOffsetScrollView.swift
@@ -0,0 +1,45 @@
+//
+//  ObserveOffsetScrollView.swift
+//  SwiftUI-Views
+//
+//  Created by Hangyeol on 11/8/23.
+//
+
+import SwiftUI
+
+struct ObserveOffsetScrollView<Content>: View where Content: View {
+    var axes: Axis.Set = .vertical
+    var showsIndicators: Bool = true
+    @Binding var offset: CGPoint
+    @ViewBuilder var content: () -> Content
+    
+    @Namespace private var coordinateSpaceName
+    
+    var body: some View {
+        ScrollView(axes, showsIndicators: showsIndicators) {
+            content()
+                .background(
+                    GeometryReader { proxy in
+                        Color.clear
+                            .preference(key: OffsetPreferenceKey.self, value: proxy.frame(in: .named(coordinateSpaceName)).origin)
+                    }
+                )
+                .onPreferenceChange(OffsetPreferenceKey.self) { offset in
+                    self.offset = CGPoint(x: -offset.x, y: -offset.y)
+                }
+        }
+        .coordinateSpace(.named(coordinateSpaceName))
+    }
+}
+
+private struct OffsetPreferenceKey: PreferenceKey {
+    static var defaultValue: CGPoint = .zero
+    
+    static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) {}
+}
+
+#Preview {
+    ObserveOffsetScrollView(offset: .constant(.zero)) {
+        Text("Preview")
+    }
+}
